c4e83d3f324ea79d67217e2750a92be457732a07
Lucas Amos <ldja@st-andrews.ac.uk>
2017-10-19 15:12:26 +0100

    tweet`

--- tweeter.py
+++ tweeter.py
@@ -34,7 +34,9 @@ def user_tweet(thandle):
     statuses = api.GetUserTimeline(screen_name=thandle)
 
     text = statuses[0].text
-    #text = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore')
+    text2 = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore')
+
+    print(type(text2))
 
     tweet = {'createdAt': statuses[0].created_at, 'text': text}
 
