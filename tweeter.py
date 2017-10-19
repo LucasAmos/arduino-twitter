ac74ac8f04a87fcd80f56ae33a4c00245b3587f0
Lucas Amos <ldja@st-andrews.ac.uk>
2017-10-19 15:39:19 +0100

    tweet`

--- tweeter.py
+++ tweeter.py
@@ -39,7 +39,7 @@ def user_tweet(thandle):
 
     ts = time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(statuses[0].created_at, '%a %b %d %H:%M:%S +0000 %Y'))
 
-    print(ts.type)
+    print(type(ts))
     tweet = {'createdAt': statuses[0].created_at, 'text': text}
 
 
