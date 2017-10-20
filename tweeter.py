286a643a1c3637dfd375df3daf21867a47955d68
Lucas Amos <ldja@st-andrews.ac.uk>
2017-10-20 09:45:11 +0100

    status text to fulltext

--- tweeter.py
+++ tweeter.py
@@ -33,8 +33,10 @@ api = twitter.Api(
 def user_tweet(thandle):
     statuses = api.GetUserTimeline(screen_name=thandle)
 
-    text = statuses[0].full_text
-    #text = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode("utf-8")
+    print(statuses[0])
+
+    text = statuses[0].text
+    text = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode("utf-8")
 
     ts = time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(statuses[0].created_at, '%a %b %d %H:%M:%S +0000 %Y'))
     ts = time.strptime(ts,'%Y-%m-%d %H:%M:%S')
